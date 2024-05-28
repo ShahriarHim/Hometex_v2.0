7bb646ed6469347efeaef0f49357bbd997f19af6
MD ASIFUL ALAM <66010854+Muhammad-Asif0@users.noreply.github.com>
2024-05-28 15:18:42 +0600

    toggle to top ix

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,11 @@
 import React, { useState, useEffect, useCallback, Suspense } from "react";
 import { BsFillArrowUpCircleFill } from 'react-icons/bs';
+import { FaWhatsapp, FaPhoneAlt } from 'react-icons/fa'; // Importing Phone and WhatsApp icons
 
 import MainSlider from "@/components/home/MainSlider";
-import BannerTop from "@/components/home/BannerTop";
-import { Policies } from "@/components/home/Policies";
-import Prefooter from "@/components/layout/Prefooter";
-import ProductsTabs from "@/components/home/ProductsTabs";
-import BoxBanner from "@/components/home/BoxBanner";
-import ComfortInBag from "@/components/home/ComfortInBag";
-import Satranji from "@/components/home/Satranji";
-import NewArrivales from "@/components/home/NewArrivales";
-import Comfoters from "@/components/home/Comfoters";
-import Curtains from "@/components/home/Curtains";
-import Exclusive from "@/components/home/Exclusive";
-import FeaturedCollection from "@/components/home/FeaturedCollection";
-import BathSupport from "@/components/home/BathSupport";
-import Blog from "@/components/home/Blog";
-import Pillow from "@/components/home/Pillow";
-import { Sticky } from "@/components/home/Sticky";
 import Constants from "@/ults/Constant";
 import DesignSix from "@/components/newDesigns/DesignSix";
 import DesignOne from "@/components/newDesigns/DesignOne";
-import DesignTwo from "@/components/newDesigns/DesignTwo";
 import DesignFifteen from "@/components/newDesigns/DesignFifteen";
 import DesignEight from "@/components/newDesigns/DesignEight";
 import DesignThree from "@/components/newDesigns/DesignThree";
@@ -32,6 +16,7 @@ import DesignFour from "@/components/newDesigns/DesignFour";
 import DesignNine from "@/components/newDesigns/DesignNine";
 import DesignTwelve from "@/components/newDesigns/DesignTwilve";
 import Prefooter2 from "@/components/layout/Prefooter2";
+import ProductsTabs from "@/components/home/ProductsTabs";
 
 const Home = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -76,46 +61,53 @@ const Home = () => {
   return (
     <>
       <MainSlider />
+      
       {isVisible && (
-        <button
-          className="fixed bottom-4 right-4 px-4 py-4 bg-green-500 text-white rounded-full shadow-md transition duration-300 hover:bg-green-600"
-          onClick={handleBackToTop}
-        >
-          <BsFillArrowUpCircleFill size={32} />
-        </button>
+        <>
+          <button
+            className="fixed bottom-5 right-4 px-4 py-4 bg-green-500 text-white rounded-full shadow-md transition duration-300 hover:bg-green-600"
+            onClick={handleBackToTop}
+          >
+            <BsFillArrowUpCircleFill size={18} />
+          </button>
+
+      
+          <button
+            className="fixed bottom-16 right-4 px-4 py-4 bg-green-500 text-white rounded-full shadow-md transition duration-300 hover:bg-green-600"
+          >
+            <FaWhatsapp size={18} onClick={() => { /* Add your WhatsApp functionality here */ }} />
+          </button>
+        </>
       )}
+     {isVisible && (
+        <>
+ 
+          <button
+            className="fixed bottom-28 right-4 px-4 py-4 bg-green-500 text-white rounded-full shadow-md transition duration-300 hover:bg-green-600"
+          >
+            <FaPhoneAlt size={18} onClick={() => { /* Add your phone functionality here */ }} />
+          </button>
+
+   
+        </>
+      )}
+  
       <DesignSix/>
       <DesignOne/>
       {/* <DesignTwo/> */}
       <Suspense fallback={<h1>loading</h1>}>
-      <ProductsTabs products={products} />
-      <DesignFifteen/>
-      <DesignEight/>
-      <DesignThree/>
-      <DesignFive/>
-      <DesignSeven/>
-      <DesignEleven/>
-      <DesignFour/>
-      <DesignNine/>
-      <DesignTwelve/>
-      <Prefooter2/>
-      {/* <BannerTop /> */}
-      {/* <Sticky /> */}
-      {/* <Policies /> */}
-      
-      {/* <BoxBanner products={products} /> */}
-      {/* <ComfortInBag products={products} /> */}
-      {/* <Satranji products={products} /> */}
-      {/* <NewArrivales products={products} /> */}
-      {/* <Comfoters products={products} /> */}
-      {/* <Curtains products={products} /> */}
-      {/* <Exclusive products={products} /> */}
-      {/* <FeaturedCollection products={products} /> */}
-      {/* <BathSupport products={products} /> */}
-      {/* <Blog /> */}
-      {/* <Pillow products={products} /> */}
+        <ProductsTabs products={products} />
+        <DesignFifteen/>
+        <DesignEight/>
+        <DesignThree/>
+        <DesignFive/>
+        <DesignSeven/>
+        <DesignEleven/>
+        <DesignFour/>
+        <DesignNine/>
+        <DesignTwelve/>
+        <Prefooter2/>
       </Suspense>
-      {/* <Prefooter /> */}
     </>
   );
 };
