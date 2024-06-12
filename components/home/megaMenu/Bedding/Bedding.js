023e3d2f7790f3c1aea652a2158e5c35347c00d1
MD ASIFUL ALAM <md.asiful.alam@g.bracu.ac.bd>
2024-06-13 01:24:50 +0600

    Update Bedding.js

--- components/home/megaMenu/Bedding/Bedding.js
+++ components/home/megaMenu/Bedding/Bedding.js
@@ -135,63 +135,62 @@ import Link from "next/link";
 import { useState } from "react";
 import { FaCaretDown, FaBed } from "react-icons/fa";
 import { useRouter } from "next/router";
-
+const sections = [
+  {
+    id: 1,
+    imageUrl: "/images/menuCategories/Bed Sheet Category-100x100.webp",
+    title: "Bed Sheet",
+    listItems: [
+      { name: "King", path: "/all-categories/bedding/bed-sheet/king" },
+      { name: "Extra King", path: "/all-categories/bedding/bed-sheet/extra-king" },
+      { name: "Semi Double", path: "/all-categories/bedding/bed-sheet/semi-double" },
+      { name: "Single", path: "/all-categories/bedding/bed-sheet/single" },
+    ],
+  },
+  {
+    id: 2,
+    imageUrl: "/images/menuCategories/Bed Cover Category-100x100.webp",
+    title: "Bed Cover",
+    listItems: [
+      { name: "Extra King size", path: "/all-categories/bedding/bed-cover/extra-king-size" },
+      { name: "King size", path: "/all-categories/bedding/bed-cover/king-size" },
+    ],
+  },
+  {
+    id: 3,
+    imageUrl: "/images/menuCategories/Bed Runner Category-100x100.webp",
+    title: "Bed Runner",
+    listItems: [{}],
+  },
+  {
+    id: 4,
+    imageUrl: "/images/menuCategories/p01-500x500-100x100.webp",
+    title: "Pillow||Pillow Protector",
+    listItems: [
+      { name: "Pillow Protector", path: "/all-categories/bedding/pillow/pillow-protector" },
+      { name: "Sleeping Pillow", path: "/all-categories/bedding/pillow/sleeping-pillow" },
+    ],
+  },
+  {
+    id: 5,
+    imageUrl: "/images/menuCategories/Fitted Sheet Category-100x100.webp",
+    title: "FittedSheet | Sheet Set",
+    listItems: [{}],
+  },
+  {
+    id: 6,
+    imageUrl: "/images/menuCategories/Mattress-100x100.webp",
+    title: "Mattress | Mattress Protector",
+    listItems: [
+      { name: "Mattress Topper", path: "/all-categories/bedding/mattress/mattress-topper" },
+      { name: "Premium Mattress", path: "/all-categories/bedding/mattress/premium-mattress" },
+    ],
+  },
+];
 const Bedding = () => {
   const router = useRouter();
 
-  const sections = [
-    {
-      id: 1,
-      imageUrl: "/images/menuCategories/Bed Sheet Category-100x100.webp",
-      title: "Bed Sheet",
-      listItems: [
-        { name: "King", path: "/all-categories/bedding/king" },
-        { name: "Extra King", path: "/all-categories/bedding/extra-king" },
-        { name: "Semi Double", path: "/all-categories/bedding/semi-double" },
-        { name: "Single", path: "/all-categories/bedding/single" },
-      ],
-    },
-    {
-      id: 2,
-      imageUrl: "/images/menuCategories/Bed Cover Category-100x100.webp",
-      title: "Bed Cover",
-      listItems: [
-        { name: "Extra King size", path: "/all-categories/bedding/extra-king-size" },
-        { name: "King size", path: "/all-categories/bedding/king-size" },
-      ],
-    },
-    {
-      id: 3,
-      imageUrl: "/images/menuCategories/Bed Runner Category-100x100.webp",
-      title: "Bed Runner",
-      listItems: [{ name: "Bed Runner", path: "/all-categories/bedding/bed-runner" }],
-    },
-    {
-      id: 4,
-      imageUrl: "/images/menuCategories/p01-500x500-100x100.webp",
-      title: "Pillow||Pillow Protector",
-      listItems: [
-        { name: "Pillow Protector", path: "/all-categories/bedding/pillow-protector" },
-        { name: "Sleeping Pillow", path: "/all-categories/bedding/sleeping-pillow" },
-      ],
-    },
-    {
-      id: 5,
-      imageUrl: "/images/menuCategories/Fitted Sheet Category-100x100.webp",
-      title: "FittedSheet | Sheet Set",
-      listItems: [{ name: "Sheet Set", path: "/all-categories/bedding/sheet-set" }],
-    },
-    {
-      id: 6,
-      imageUrl: "/images/menuCategories/Mattress-100x100.webp",
-      title: "Mattress | Mattress Protector",
-      listItems: [
-        { name: "Mattress Topper", path: "/all-categories/bedding/mattress-topper" },
-        { name: "Premium Mattress", path: "/all-categories/bedding/premium-mattress" },
-        { name: "Mattress Protector", path: "/all-categories/bedding/mattress-protector" },
-      ],
-    },
-  ];
+
 
   const handleButtonClick = () => {
     router.push("/bedding");
